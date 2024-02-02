9fde9705bdebc913265b0e583196f8dd0364e940
Noobieta Gamers <82405425+Noobieta-Gamerz@users.noreply.github.com>
2024-02-03 03:03:20 +0700

    Update install_me.sh

--- ubuntu/x86_64/install_me.sh
+++ ubuntu/x86_64/install_me.sh
@@ -5,7 +5,7 @@ sleep 20;
 echo " lumayan lama guys 😄"sleep 3;
 sleep 30; echo " sabar sabar sabar 😄";
 sleep 5;
-apt update > /dev/null 2>&1 && apt upgrade > /dev/null 2>&1 && apt --assume-yes install nala > /dev/null 2>&1 && nala install wget tar figlet proot && 
+apt update > /dev/null 2>&1 && echo "  be patien 😄" && apt upgrade --assume-yes > /dev/null 2>&1 && echo " Bentar lagi beres gan 😆" && apt --assume-yes install nala > /dev/null 2>&1 && nala install wget tar figlet proot && 
 wget https://github.com/Noobieta-Gamerz/Termux/raw/main/qemu-termux/qemu-x86_64
 chmod 755 qemu-x86_64
 mv qemu-x86_65 $PREFIX/bin/
