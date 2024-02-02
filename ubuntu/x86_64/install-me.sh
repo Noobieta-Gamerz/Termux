a673e62bea08f2000983cbb8e56804ac864c3330
Noobieta Gamers <82405425+Noobieta-Gamerz@users.noreply.github.com>
2024-02-02 18:20:02 +0700

    Create install-me.sh

---
+++ ubuntu/x86_64/install-me.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+echo -e "              Please waiiiitt for install package... (^_^)"
+apt update > /dev/null 2>&1 && apt upgrade > /dev/null 2>&1 && apt --assume-yes install nala > /dev/null 2>&1 && nala install figlet proot
+cd
+wget https://github.com/Noobieta-Gamerz/Termux/raw/main/qemu-termux/qemu-x86_64
+
+chmod 755 qemu-x86_64
+mv qemu-x86_65 $PREFIX/bin/
+mkdir vm-termux
+cd vm-termux
+wget https://download939.mediafire.com/na08wg9dbxsg1ZJl2kYsSG4QN6ZYyg12uDVd8mvr7GanRjeYhYjAXFpJuwuKltCCk43WVCwzhW425lP9AFQQmrqnKaQaQj7lvM874k3ouzZOLr42O0aDmPwDW3psp6PyEipKV8VnEGukIMbOs0ilYcMSFGhWMryp08eeVvg5qcn6/xci3noeg0ho6ell/ubuntu-rootfs.tar.xz
+echo ""
+echo -e "   Wait for extract (^_^) ... "
+echo ""
+tar -xJf ubuntu-rootfs.tar.xz
+
+figlet -c Finish yey
+
+echo "  $(date)"
+echo -e "    [•] Note : for start ubuntu : ./start-ubuntu.sh happiness day (^_^)"
