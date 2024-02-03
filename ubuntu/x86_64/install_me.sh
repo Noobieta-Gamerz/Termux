9014215933eab5cf579b17da873dbf086ca49c78
Noobieta Gamers <82405425+Noobieta-Gamerz@users.noreply.github.com>
2024-02-03 11:25:46 +0700

    Update install_me.sh

--- ubuntu/x86_64/install_me.sh
+++ ubuntu/x86_64/install_me.sh
@@ -5,7 +5,7 @@ sleep 20;
 echo " [•] lumayan lama guys 😄";sleep 3;
 sleep 30; echo " [•] sabar sabar sabar ini ujian 😄";
 sleep 5;
-apt update > /dev/null 2>&1 && echo "  [•] be patien 😄" && apt upgrade --assume-yes > /dev/null 2>&1 && echo " [•] Bentar lagi beres gan 😆" && apt --assume-yes install nala > /dev/null 2>&1 && nala install wget tar figlet proot && 
+apt update > /dev/null 2>&1 && echo " [•] be patien 😄" && apt upgrade --assume-yes > /dev/null 2>&1 && echo " [•] Bentar lagi beres gan 😆" && apt --assume-yes install nala > /dev/null 2>&1 && nala install wget tar figlet proot && 
 wget https://github.com/Noobieta-Gamerz/Termux/raw/main/qemu-termux/qemu-x86_64
 chmod 755 qemu-x86_64
 mv ~/qemu-x86_64 $PREFIX/bin/
@@ -14,6 +14,7 @@ cd ~/vm-termux
 echo ""
 echo -e "           Drive download content 😚 Be patient ....."
 echo ""
+sleep 5;
 wget --no-check-certificate -r "https://drive.usercontent.google.com/download?id=10E63Hd1QKXaHNnz5QSLF8fEgnWM9Quq1&export=download&confirm=t&uuid=79a039dd-e33b-4232-92f7-110218222d37" -O ubuntu-rootfs.tar.xz
 echo ""
 echo -e "   Wait for extract (^_^) ... "
